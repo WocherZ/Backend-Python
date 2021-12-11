a4719fb06876550ed7b8714ce885363afef187f2
Vladimir <cay108@yandex.ru>
2021-12-11 16:44:09 +0300

    Add decorators

--- Homework №6/mysite/users/views.py
+++ Homework №6/mysite/users/views.py
@@ -1,59 +1,52 @@
-from django.http import JsonResponse, HttpResponseNotAllowed
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
 
 
 def is_registered():
     return True
 
 
+@require_http_methods(["GET"])
 def user(request):
-    if request.method == "GET":
-        if is_registered():
-            return render(request, 'user.html')
-        else:
-            return redirect('registration')
-
+    if is_registered():
+        return render(request, 'user.html')
     else:
-        return HttpResponseNotAllowed(["GET"])
+        return redirect('registration')
 
 
+@require_http_methods(["GET"])
 def order(request):
-    if request.method == "GET":
-        return JsonResponse({
-            "description": "User orders page",
-            "user_orders": [
-                "order 1",
-                "order 2",
-            ]
-        })
-
-    else:
-        return HttpResponseNotAllowed(["GET"])
+    return JsonResponse({
+        "description": "User orders page",
+        "user_orders": [
+            "order 1",
+            "order 2",
+        ]
+    })
 
 
+@require_http_methods(["GET"])
 def cart(request):
-    if request.method == "GET":
-        return JsonResponse({
-            "description": "User cart page",
-            "user_cart": [
-                "product 1",
-                "product 2",
-            ]
-        })
-
-    else:
-        return HttpResponseNotAllowed(["GET"])
+    return JsonResponse({
+        "description": "User cart page",
+        "user_cart": [
+            "product 1",
+            "product 2",
+        ]
+    })
 
 
+@require_http_methods(["GET", "POST"])
 def registration(request):
     if request.method == "GET":
         return JsonResponse({
             "description": "Registration user page. There will be a form"
         })
-    elif request.method == "POST":
+
+    else:
         return JsonResponse({
             "method": "POST",
             "success": True,  # or False
         })
-    else:
-        return HttpResponseNotAllowed(["GET", "POST"])
+
